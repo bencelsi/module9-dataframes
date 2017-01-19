748bea8c9f8f199a7da307e0a197bd775bd80de2
Ben Celsi <Ben Celsi>
2017-01-19 15:21:18 -0800

    start/finish various modules

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,27 +2,32 @@
 
 # Create a vector of the number of points the Seahawks scored in each game this
 # season (google "Seahawks" for the info!)
-
+seahawks.points <- c(1, 0, 2, 0, 500)
 
 # Create a vector of the number of points the Seahwaks have allowed to be scored
 # against them in each game this season
-
+other.points <- c(144, 30, 32, 20, 51)
 
 # Combine your two vectors into a dataframe
-
+dataframe <- data.frame(seahawks.points, other.points)
 
 # Create a new column "diff" that is the difference in points.
 # Hint: recall the syntax for assigning new elements (which in this case will be
 # a vector) to a list!
 
+dataframe[["diff"]] <- seahawks.points - other.points
 
 # Create a new column "won" which is TRUE if the Seahawks won
 
+dataframe[["won"]] <- dataframe[["diff"]] > 1
 
 # Create a vector of the opponent names corresponding to the games played
 
+my.names <- c("raptors", "cowboys", "cows", "dinosaurs", "monks")
 
 # Assign your dataframe rownames of their opponents
 
+rownames(dataframe) <- my.names
 
 # View your data frame to see how it has changed!
+
--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -1,33 +1,40 @@
 # Load R's "USPersonalExpenditure" dataset using the "data()" function
 # This will produce a data frame called `USPersonalExpenditure`
 
+exp <- data.frame(USPersonalExpenditure)
 
 # The variable USPersonalExpenditure is now accessible to you. Unfortunately,
 # it's not a data frame (it's actually what is called a matrix)
 # Test this using the `is.data.frame()` function
-
+typeof(exp)
 
 # Luckily, you can simply pass the USPersonalExpenditure variable as an argument
 # to the `data.frame()` function to convert it a data farm. Do this, storing the
 # result in a new variable
-
+is.data.frame(exp)
 
 # What are the column names of your dataframe?
-
+colnames(exp)
 
 # Why are they so strange? Think about whether you could use a number like 1940
 # with dollar notation!
 
 # What are the row names of your dataframe?
-
+rownames(exp)
 
 # Create a column "category" that is equal to your rownames
-
+category <- rownames(exp)
 
 # How much money was spent on personal care in 1940?
+typeof(exp)
+typeof(exp["Personal Care", ])
 
+typeof(exp[["Personal Care"]])
+typeof(exp[["X1940"]])
+typeof(exp[])
 
 # How much money was spent on Food and Tobacco in 1960?
+exp[["Food and Tobacco", "X1960"]]
 
 
 # What was the highest expenditure category in 1960?
